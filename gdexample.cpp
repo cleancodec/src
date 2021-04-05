2981107bd9b84cb321db793797892d3487f0e042
Lo-oker <42920029+cleancodec@users.noreply.github.com>
2021-04-05 13:31:17 +0530

    intermediate success build

--- gdexample.cpp
+++ gdexample.cpp
@@ -1,19 +1,49 @@
+
+
 #include "gdexample.hpp"
 
 using namespace godot;
 
 void GDExample::_register_methods() {
 
+
     register_method("a_method", &GDExample::a_method);
+
+    //register_property<GDExample, String>("data", &GDExample::_data, String("Hello World"));
+    //register_property<GDExample, float>("amplitude", &GDExample::amplitude, 10.0);  
+
+    register_property<GDExample, float>("amplitude", &GDExample::amplitude, 10.0);
+
 }
 
 void GDExample::_init() {
-    // initialize any variables here
+
+
+    time_passed = 0.0; //
+    amplitude = 10.0;   //
 }
 
+void GDExample::set_data(String new_data) {
+    _data = new_data;
+}
+String GDExample::get_data() const {
+    return _data;
+}
+/*
 String GDExample::a_method() {
 
-    String text = "Hello World!";
+    return _data;
+}
+*/
 
-    return text;
+String GDExample::a_method(float delta) {
+
+    time_passed += delta;
+
+    Vector2 new_position = Vector2(
+        amplitude + (amplitude * sin(time_passed * 2.0)),
+        amplitude + (amplitude * cos(time_passed * 1.5))
+    );
+
+    return new_position;
 }
--- gdexample.hpp
+++ gdexample.hpp
@@ -8,6 +8,10 @@ namespace godot {
 
     class GDExample : public Node {
         GODOT_CLASS(GDExample, Node)
+    private:
+        String _data;
+        float time_passed;
+        float amplitude;
 
     public:
         static void _register_methods();
@@ -15,9 +19,14 @@ namespace godot {
 
         void _init(); // our initializer called by Godot
 
-        String a_method();
+        void set_data(String new_data);
+        String get_data() const;
+
+        String a_method(float delta);
     };
 
 }
 
 #endif  /* GDEXAMPLE_H */
+
+
